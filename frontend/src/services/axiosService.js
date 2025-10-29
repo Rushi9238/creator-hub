c272f4b2bde8a04540f702b1e28e0cb0a2965a59
Rushikesh Patil <rushikesh.patil@nulou.in>
2025-10-29 11:37:31 +0530

    axios base api url chnage

--- frontend/src/services/axiosService.js
+++ frontend/src/services/axiosService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create axios instance with base configuration
 export const api = axios.create({
-  baseURL: "http://localhost:3044/api",
+  baseURL: "/",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
